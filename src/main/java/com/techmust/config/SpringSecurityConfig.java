7edee0882b0a075df99ca674309c662dc7c0448a
rakshan <rakshan@techmust.com>
2018-08-13 15:34:34 +0530

    changes made in configuration file

---
+++ src/main/java/com/techmust/config/SpringSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.techmust.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@PropertySource("classpath:resources/application.properties")
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter
+{  
+	
+}
