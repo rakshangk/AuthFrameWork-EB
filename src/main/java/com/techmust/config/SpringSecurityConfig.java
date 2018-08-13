277a016a01de08a598faab2976949e169b60f95f
rakshan <rakshan@techmust.com>
2018-08-13 16:57:00 +0530

    reverted changes, removed spring-boot-starter-security dependency in pom

--- src/main/java/com/techmust/config/SpringSecurityConfig.java
+++ src/main/java/com/techmust/config/SpringSecurityConfig.java
@@ -1,10 +1,5 @@
 package com.techmust.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:resources/application.properties")
 public class SpringSecurityConfig 
 {  
 	
