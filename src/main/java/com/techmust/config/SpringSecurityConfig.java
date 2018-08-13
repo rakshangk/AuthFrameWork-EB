6032eea00c9268498733c210a670c817c6d37a44
rakshan <rakshan@techmust.com>
2018-08-13 18:31:19 +0530

    Imported properties to config file

--- src/main/java/com/techmust/config/SpringSecurityConfig.java
+++ src/main/java/com/techmust/config/SpringSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.techmust.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:resources/application.properties")
 public class SpringSecurityConfig
 {  
 	
