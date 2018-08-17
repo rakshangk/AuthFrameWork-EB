c66bcd11a86078cd4687f26b2fff191ce289faf2
rakshan <rakshan@techmust.com>
2018-08-17 12:56:38 +0530

    updated context path

--- src/main/java/com/techmust/Application.java
+++ src/main/java/com/techmust/Application.java
@@ -22,7 +22,7 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args)
     {
-    	System.setProperty("server.servlet.ContextPath", "/techmust");
+    	//System.setProperty("server.servlet.ContextPath", "/techmust");
         SpringApplication.run(Application.class, args);
     }   
 }
